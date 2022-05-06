dbc70d02402bbb6f25a75b856799e8da78a2e315
ayn030 <kojinco18.033@gmail.com>
2022-05-06 15:56:44 +0900

    フォーマットの修正

--- src/main/java/com/example/demo/entity/DateCalc.java
+++ src/main/java/com/example/demo/entity/DateCalc.java
@@ -9,19 +9,19 @@ import lombok.Data;
 public class DateCalc {
 
 	private Integer id;
-	
+
 	@NotBlank(message = "日付IDを入力してください")
 	private String dateid;
-	
+
 	@NotBlank(message = "日付名を入力してください")
 	private String name;
-	
+
 	@NotNull(message = "1桁以上の数値を入力してください")
 	private Integer plusyear;
-	
+
 	@NotNull(message = "1桁以上の数値を入力してください")
 	private Integer plusmonth;
-	
+
 	@NotNull(message = "1桁以上の数値を入力してください")
 	private Integer plusday;
 }
