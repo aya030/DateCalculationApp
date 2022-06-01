97fd90f9c176247477b5a0ffd6a89fb6668814f5
ayn030 <kojinco18.033@gmail.com>
2022-06-01 14:18:32 +0900

    RequestFormのフィールドをprivateに変更。に伴いServiceの編集

--- src/main/java/com/example/demo/service/DateService.java
+++ src/main/java/com/example/demo/service/DateService.java
@@ -50,7 +50,7 @@ public class DateService {
 	// 計算
 	public List<LocalDate> calculationDate(RequestForm requestForm) {
 
-		String inputDate = requestForm.inputDate;
+		String inputDate = requestForm.getInputDate();
 		LocalDate selectedDate = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 		List<DateCalc> dateCalcs = dateMapper.findAll();
 
